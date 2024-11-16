f00ee21bdb8b861c26cff5d6f7900977e276bb83
FuadHmdn <fuadhamidan.drive@gmail.com>
2024-11-16 22:08:22 +0700

    add latar belakang menu

--- app/src/main/java/com/fuad/kidneyfit/ui/LatarBelakangActivity.kt
+++ app/src/main/java/com/fuad/kidneyfit/ui/LatarBelakangActivity.kt
@@ -1,15 +1,30 @@
 package com.fuad.kidneyfit.ui
 
+import android.os.Build
 import android.os.Bundle
+import android.text.Layout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.fuad.kidneyfit.R
+import com.fuad.kidneyfit.databinding.ActivityLatarBelakangBinding
 
 class LatarBelakangActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLatarBelakangBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_latar_belakang)
+        binding = ActivityLatarBelakangBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            binding.materiLatarBelakang.justificationMode = Layout.JUSTIFICATION_MODE_INTER_WORD
+        }
+
+        binding.backBtn.setOnClickListener {
+            finish()
+        }
     }
 }
