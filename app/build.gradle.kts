abadd5f047fca29ff80c91b16be845557fe33b12
FuadHmdn <fuadhamidan.drive@gmail.com>
2024-11-16 21:28:17 +0700

    create menu home

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.fuad.kidneyfit"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.fuad.kidneyfit"
@@ -34,7 +34,7 @@ android {
         jvmTarget = "1.8"
     }
     buildFeatures {
-        viewBinding
+        viewBinding = true
     }
 }
 
---
+++ app/src/main/java/com/fuad/kidneyfit/ui/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.fuad.kidneyfit.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fuad.kidneyfit.R
+import com.fuad.kidneyfit.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setListener()
+    }
+
+    private fun setListener() {
+        binding.latarBelakang.setOnClickListener {
+            val intent = Intent(this, LatarBelakangActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
